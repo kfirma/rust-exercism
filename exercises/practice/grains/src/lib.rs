f4b5e3d2495077604d72df6037ec7da620a37ead
kfirma <kkfir1708@gmail.com>
2022-10-02 12:26:54 +0300

    grains

--- exercises/practice/grains/src/lib.rs
+++ exercises/practice/grains/src/lib.rs
@@ -1,7 +1,18 @@
+const SQUARE_OUT_OF_RANGE: &str = "Square must be between 1 and 64";
+const MIN_SQUARE: u64 = 1;
+const MAX_SQUARE: u64 = 64;
+const QUOTIENT: u64 = 2;
+
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if !(MIN_SQUARE..=MAX_SQUARE).contains(&(u64::from(s))) {
+        panic!("{}", SQUARE_OUT_OF_RANGE);
+    }
+    QUOTIENT.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    let a1 = u128::from(MIN_SQUARE);
+    let n = MAX_SQUARE as u32;
+
+    (a1 * (u128::from(QUOTIENT).pow(n) - 1) / u128::from(QUOTIENT - 1)) as u64
 }
