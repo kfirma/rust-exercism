9458e52e939a9142a3e0de482882d3d947aaa4fe
kfirma <kkfir1708@gmail.com>
2022-10-03 09:46:39 +0300

    as instead of from, deleted const str

--- exercises/practice/grains/src/lib.rs
+++ exercises/practice/grains/src/lib.rs
@@ -1,18 +1,17 @@
-const SQUARE_OUT_OF_RANGE: &str = "Square must be between 1 and 64";
 const MIN_SQUARE: u64 = 1;
 const MAX_SQUARE: u64 = 64;
 const QUOTIENT: u64 = 2;
 
-pub fn square(s: u32) -> u64 {
-    if !(MIN_SQUARE..=MAX_SQUARE).contains(&(u64::from(s))) {
-        panic!("{}", SQUARE_OUT_OF_RANGE);
+pub fn square(square_number: u32) -> u64 {
+    if !(MIN_SQUARE..=MAX_SQUARE).contains(&(square_number as u64)) {
+        panic!("Square must be between {} and {}", MIN_SQUARE, MAX_SQUARE);
     }
-    QUOTIENT.pow(s - 1)
+    QUOTIENT.pow(square_number - 1)
 }
 
 pub fn total() -> u64 {
-    let a1 = u128::from(MIN_SQUARE);
+    let a1 = MIN_SQUARE as u128;
     let n = MAX_SQUARE as u32;
 
-    (a1 * (u128::from(QUOTIENT).pow(n) - 1) / u128::from(QUOTIENT - 1)) as u64
+    (a1 * ((QUOTIENT as u128).pow(n) - 1) / (QUOTIENT as u128 - 1)) as u64
 }
